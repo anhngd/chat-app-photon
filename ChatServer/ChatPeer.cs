5f8198ace464ecf52ea41740b91c742414ecc157
Nguyen Anh <anhnd@bravestars.com>
2020-08-31 18:59:00 +0700

    Initial commit

---
+++ ChatServer/ChatPeer.cs
@@ -0,0 +1,39 @@
+﻿using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+using System;
+
+public class ChatPeer : ClientPeer
+{
+    private static event Action<ChatPeer, EventData, SendParameters> BroadcastMessage;
+    public ChatPeer(InitRequest initRequest)
+    : base(initRequest)
+    {
+        BroadcastMessage += OnBroadcastMessage;
+    }
+
+    protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+    {
+        BroadcastMessage -= OnBroadcastMessage;
+    }
+
+    protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+    {
+        if (operationRequest.OperationCode == 1) // Chat Custom Operation Code = 1
+        {
+            // broadcast chat custom event to other peers
+            var eventData = new EventData(1) { Parameters = operationRequest.Parameters }; // Chat Custom Event Code = 1
+            BroadcastMessage(this, eventData, sendParameters);
+            // send operation response (~ACK) back to peer
+            var response = new OperationResponse(operationRequest.OperationCode);
+            SendOperationResponse(response, sendParameters);
+        }
+    }
+
+    private void OnBroadcastMessage(ChatPeer peer, EventData eventData, SendParameters sendParameters)
+    {
+        if (peer != this) // do not send chat custom event to peer who called the chat custom operation
+        {
+            SendEvent(eventData, sendParameters);
+        }
+    }
+}
---
+++ ChatServer/ChatServer.cs
@@ -0,0 +1,19 @@
+﻿using Photon.SocketServer;
+using System;
+public class ChatServer : ApplicationBase
+{
+    protected override PeerBase CreatePeer(InitRequest initRequest)
+    {
+        return new ChatPeer(initRequest);
+    }
+
+    protected override void Setup()
+    {
+
+    }
+
+    protected override void TearDown()
+    {
+        Console.WriteLine("[TearDown] Server is stopped!!!!!");
+    }
+}
